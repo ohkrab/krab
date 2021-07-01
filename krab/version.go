6313c9d6b781cc1c9b391ee0c362e8c3103d3ac7
qbart <wojtowicz.bartlomiej@gmail.com>
2021-07-02 00:52:04 +0200

    Auto releaser

--- krab/version.go
+++ krab/version.go
@@ -3,6 +3,11 @@ package krab
 const (
 	InfoName     = "krab"
 	InfoFullName = "Oh Krab!"
-	InfoVersion  = "0.2.0"
 	InfoWWW      = "https://ohkrab.dev"
+)
+
+var (
+	InfoVersion   = "<generated>"
+	InfoCommit    = "<generated>"
+	InfoBuildDate = "<generated>"
 )
